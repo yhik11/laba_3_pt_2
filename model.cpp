3de376896d560437b1ef122835afa55253642bf4
Vildan <miniakhmeto@mail.ru>
2025-12-10 17:11:42 +0500

    Добавление сохранения и загрузки модели из файла

--- model.cpp
+++ model.cpp
@@ -1,5 +1,7 @@
 #include "model.h"
 #include <algorithm>
+#include <QFile>
+#include <QTextStream>
 
 Model::Model() : a(25), b(50), c(75) {
 }
@@ -43,4 +45,32 @@ void Model::setC(int value) {
 
     c = newC;
     emit dataChanged();
+}
+
+void Model::saveToFile(const QString& filename) {
+    QFile file(filename);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << a << " " << b << " " << c;
+        file.close();
+    }
+}
+
+void Model::loadFromFile(const QString& filename) {
+    QFile file(filename);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&file);
+        in >> a >> b >> c;
+        file.close();
+
+        a = std::max(0, std::min(a, 100));
+        b = std::max(0, std::min(b, 100));
+        c = std::max(0, std::min(c, 100));
+
+        if (a > b) std::swap(a, b);
+        if (b > c) std::swap(b, c);
+        if (a > b) std::swap(a, b);
+
+        emit dataChanged();
+    }
 }
--- model.h
+++ model.h
@@ -1,10 +1,30 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-class model
-{
+#include <QObject>
+
+class Model : public QObject {
+    Q_OBJECT
+
+private:
+    int a, b, c;
+
 public:
-    model();
+    Model();
+
+    int getA() const { return a; }
+    int getB() const { return b; }
+    int getC() const { return c; }
+
+    void setA(int value);
+    void setB(int value);
+    void setC(int value);
+
+    void saveToFile(const QString& filename);
+    void loadFromFile(const QString& filename);
+
+signals:
+    void dataChanged();
 };
 
-#endif // MODEL_H
+#endif
